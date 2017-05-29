ada55bbf43a851c820fb2d8cdfe4ca672c1b4013
hilmakh <hilmakh@hotmail.com>
2017-05-28 17:23:00 -0700

    updates

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ client.on('ready', () => {
 var prefix = "?";
 
 client.on('message', msg => {
-    if(msg.author === client.user) return;
-    if(msg.content.startsWith(prefix)) {
-        var command = msg.content;
-        if(command === "?ping"){
-            msg.reply('Pong!');
-        }
+    let args = msg.content.split(' ').slice(1);
+    let argsOrig = msg.content.split(' ');
+    var result = args.join(' ');
+    var command = argsOrig[0].split('').slice(1).join('').toLowerCase();
+
+    if(!msg.content.startsWith(prefix)) return;
+    if(msg.author.bot) return;
+
+    if(command === "ping") {
+        msg.channel.send(`Pong! \`${Date.now() - msg.createdTimestamp}ms\``);
     }
+
 });
 
 client.login(token.token);
